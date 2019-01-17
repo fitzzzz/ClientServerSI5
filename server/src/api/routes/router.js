22bf3dc98b4244a0f18ade1f71b554831463eaa3
Guillaume André <guillaume.andre@etu.unice.fr>
2019-01-17 11:00:08 +0100

    Separate server

--- server/src/api/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const searchRouter = require("./search.router");
-
-const router = express.Router();
-
-router.get("/foods", searchRouter.searchFood);
-router.post("/foods/:id", searchRouter.createFood);
-
-module.exports = router;
--- server/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while ! curl http://db:27017/ 2>&1 | grep '52'
-do
-  echo "Failed to connect to database, attempting again in 5 seconds"
-  sleep 5
-done
-npm start
