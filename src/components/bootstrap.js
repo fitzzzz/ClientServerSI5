c632b8aaecc8bbc9bd498159866e85d92d924753
JA <jean-adam.puskaric@etu.unice.fr>
2019-01-21 16:32:11 +0100

    Fixed shit

---
+++ src/components/bootstrap.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+import bContainer from 'bootstrap-vue/es/components/layout/container'
+import bRow from 'bootstrap-vue/es/components/layout/row'
+import bCol from 'bootstrap-vue/es/components/layout/col'
+import bJumbotron from 'bootstrap-vue/es/components/jumbotron/jumbotron'
+import bButton from 'bootstrap-vue/es/components/button/button'
+import bNavbar from 'bootstrap-vue/es/components/navbar/navbar'
+import bNavbarT from 'bootstrap-vue/es/components/navbar/navbar-toggle'
+import bNavbarB from 'bootstrap-vue/es/components/navbar/navbar-brand'
+import bNavbarNav from 'bootstrap-vue/es/components/navbar/navbar-nav'
+import bNavItem from 'bootstrap-vue/es/components/nav/nav-item'
+import bCollapse from 'bootstrap-vue/es/components/collapse/collapse'
+
+Vue.component('b-container', bContainer);
+Vue.component('b-col', bCol);
+Vue.component('b-row', bRow);
+Vue.component('b-jumbotron', bJumbotron);
+Vue.component('b-btn', bButton);
+Vue.component('b-navbar', bNavbar);
+Vue.component('b-navbar-toggle', bNavbarT);
+Vue.component('b-navbar-brand', bNavbarB);
+Vue.component('b-navbar-nav', bNavbarNav);
+Vue.component('b-nav-item', bNavItem);
+Vue.component('b-collapse', bCollapse);
--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
 import vSelect from 'vue-select'
+import './components/bootstrap'
 
 import router from './router'
 
 Vue.component('v-select', vSelect);
 Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue);
+//Vue.use(BootstrapVue);
 
 new Vue({
     router,
