3e587055a436c519ce0d73b501eb2e17fc2a0dae
Zaki <berouk.zaki@gmail.com>
2019-01-28 15:50:21 +0100

    display info in a table; responsive; scroll; sick; swagg;

--- src/components/sub/FoodInfo.vue
+++ src/components/sub/FoodInfo.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-        <p>{{food.name}}</p>
-        <div v-if="food.ingredients.length > 0"><p>{{food.ingredients}}</p></div>
-        <div v-else><img :src="getRandomIcon()" width="50%" height="50%" alt="No Ingredient Found"/>
+    <div class="food-info-container">
+        <h4>{{food.name}}</h4>
+        <div v-if="food.ingredients.length > 0">
+            <table class="table table-hover table-wrapper-scroll-y table-striped">
+                <thead>
+                <tr>
+                    <th scope="col" class="py-2">Composant</th>
+                    <th scope="col" class="py-2">Pourcentage</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item in food.ingredients" :key="item.id">
+                    <th scope="row" class="py-2">{{item.text}}</th>
+                    <td v-if="item.percent != null" class="py-2">{{item.percent}}%</td>
+                    <td v-if="item.percent == null" class="py-2"> -</td>
+                </tr>
+
+                </tbody>
+            </table>
         </div>
+        <div v-else class="no-information">
+            <img :src="getRandomIcon()" width="70%" height="70%" alt="No Ingredient Found"/>
+            <div class="row no-info-message">
+                <p class="lead">Whoopsie ! <br/>Nous n'avons aucune information concernant ce produit...</p>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -22,7 +44,42 @@
         methods: {
             getRandomIcon() {
                 return require('../../assets/' + gif_list[getRandomInt(0, 2)] + '.gif');
-            }
+            },
         }
     }
 </script>
+
+<style>
+
+    .food-info-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h4 {
+        margin-bottom: 30px;
+        margin-top: 30px;
+    }
+
+    .no-information {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .no-info-message {
+        margin-top: 20px;
+    }
+
+    .table-wrapper-scroll-y {
+        display: block;
+        max-height: 400px;
+        overflow-y: auto;
+        -ms-overflow-style: -ms-autohiding-scrollbar;
+        width: 100%;
+    }
+
+</style>
